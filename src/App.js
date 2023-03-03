98076d1c1b795a0c9f5bd9016223d1341a944419
Studawatta <st.udawatte@gmail.com>
2023-03-03 21:02:49 +0530

    update 03.03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import SearchButton from './components/buttons/search_button';
+import CheckInOut from './components/inputs/search_inputs/check_in_out';
+import LocationInput from './components/inputs/search_inputs/location';
 import Rooms from './components/inputs/search_inputs/rooms';
 
 const App = () => {
   return (
     <div className="a">
       <div className="aa">
+        <LocationInput />
+        <CheckInOut type="Check-in Date" />
+        <CheckInOut type="Check-out Date" />
         <Rooms />
+        <SearchButton />
       </div>
     </div>
   );
--- src/components/inputs/search_inputs/rooms.js
+++ src/components/inputs/search_inputs/rooms.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+
 import './roomsStyle.css';
 
 const Rooms = () => {
-  const [showInputs, setShowInputs] = useState(true);
+  const [showInputs, setShowInputs] = useState(false);
+
+  const [adultsCount, setAdultsCount] = useState(1);
+  const [childrenCount, setChildrenCount] = useState(0);
+  const [roomsCount, setRoomsCount] = useState(1);
+
   return (
     <div>
       <div
@@ -11,35 +17,90 @@ const Rooms = () => {
           setShowInputs(!showInputs);
         }}
       >
-        0 adults 0 children 0 rooms
+        {adultsCount} adults {childrenCount} children {roomsCount} rooms
       </div>
       {showInputs ? (
         <div className="inputCont">
           <div className="cont">
             <span>Adults</span>
             <div className="changeValCont">
-              <div className="decrease"> -</div>
-              <span className="val">1</span>
-              <div className="increase">+</div>
+              <div
+                className={`${adultsCount === 1 ? 'notAllowed' : 'decrease'}`}
+                onClick={() => {
+                  if (adultsCount > 1) {
+                    setAdultsCount(adultsCount - 1);
+                  }
+                }}
+              >
+                -
+              </div>
+              <span className="val">{adultsCount}</span>
+              <div
+                className="increase"
+                onClick={() => {
+                  setAdultsCount(adultsCount + 1);
+                }}
+              >
+                +
+              </div>
             </div>
           </div>
           <div className="cont">
             <span>children</span>
             <div className="changeValCont">
-              <div className="decrease"> -</div>
-              <span className="val">1</span>
-              <div className="increase">+</div>
+              <div
+                className={`${childrenCount <= 0 ? 'notAllowed' : 'decrease'}`}
+                onClick={() => {
+                  if (childrenCount > 0) {
+                    setChildrenCount(childrenCount - 1);
+                  }
+                }}
+              >
+                -
+              </div>
+              <span className="val">{childrenCount}</span>
+              <div
+                className="increase"
+                onClick={() => {
+                  setChildrenCount(childrenCount + 1);
+                }}
+              >
+                +
+              </div>
             </div>
           </div>
           <div className="cont">
             <span>Rooms</span>
             <div className="changeValCont">
-              <div className="decrease"> -</div>
-              <span className="val">1</span>
-              <div className="increase">+</div>
+              <div
+                className={`${roomsCount <= 1 ? 'notAllowed' : 'decrease'}`}
+                onClick={() => {
+                  if (roomsCount > 1) {
+                    setRoomsCount(roomsCount - 1);
+                  }
+                }}
+              >
+                -
+              </div>
+              <span className="val">{roomsCount}</span>
+              <div
+                className="increase"
+                onClick={() => {
+                  setRoomsCount(roomsCount + 1);
+                }}
+              >
+                +
+              </div>
             </div>
           </div>
-          <div className="confirmation">Done</div>
+          <div
+            className="confirmation"
+            onClick={() => {
+              setShowInputs(false);
+            }}
+          >
+            Done
+          </div>
         </div>
       ) : (
         ''
