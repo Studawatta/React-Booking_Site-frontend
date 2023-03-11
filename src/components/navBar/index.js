ecd168f89f464bc49dfbb1051cf8bab60c8730b9
Studawatta <st.udawatte@gmail.com>
2023-03-11 23:21:43 +0530

    03.11

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import HeaderButton from '../buttons/header_buttons';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 
 import './navStyle.css';
 import EntryButton from '../buttons/entry_buttons';
-import ServiceButton from '../buttons/services_buttons';
+import LocationInput from '../inputs/search_inputs/location';
+import CheckInOut from '../inputs/search_inputs/check_in_out';
+import Rooms from '../inputs/search_inputs/rooms';
+import SearchButton from '../buttons/search_button';
+import { MdBed } from 'react-icons/md';
 
 const NavBar = () => {
+  const [currentPage, setCurrentPage] = useState('stays');
   return (
     <Router>
       <div className="navWrapper">
@@ -24,14 +29,65 @@ const NavBar = () => {
             </div>
           </div>
           <div className="raw_2">
-            <ServiceButton
-              type="Stays"
-              id="firstBtn"
-            />
-            <ServiceButton type="Flights" />
-            <ServiceButton type="Car rentals" />
-            <ServiceButton type="Attractions" />
-            <ServiceButton type="Airport taxis" />
+            <div
+              className={`${currentPage === 'stays' ? 'cPage' : 'page'}`}
+              onClick={() => {
+                setCurrentPage('stays');
+              }}
+            >
+              <MdBed className="text-[20px] mr-[4px] " />
+              Stays
+            </div>
+            <div
+              className={`${currentPage === 'flights' ? 'cPage' : 'page'}`}
+              onClick={() => {
+                setCurrentPage('flights');
+              }}
+            >
+              Flights
+            </div>
+            <div
+              className={`${currentPage === 'carRentals' ? 'cPage' : 'page'}`}
+              onClick={() => {
+                setCurrentPage('carRentals');
+              }}
+            >
+              Car rentals
+            </div>
+            <div
+              className={`${currentPage === 'attractions' ? 'cPage' : 'page'}`}
+              onClick={() => {
+                setCurrentPage('attractions');
+              }}
+            >
+              Attractions
+            </div>
+            <div
+              className={`${currentPage === 'airportTaxis' ? 'cPage' : 'page'}`}
+              onClick={() => {
+                setCurrentPage('airportTaxis');
+              }}
+            >
+              Airport taxis
+            </div>
+          </div>
+          <div className="raw_3">
+            <h1 className="text-[40px] font-bold text-white mt-[80px]">
+              Find your next stay
+            </h1>
+            <h3 className="text-[24px] font-semibold text-white">
+              Search low prices on hotels, homes and much more...
+            </h3>
+          </div>
+          <div className="searchBar">
+            <LocationInput />
+            <div className="flex">
+              <CheckInOut type="Check-in" />
+              <CheckInOut type="Check-out" />
+            </div>
+
+            <Rooms />
+            <SearchButton />
           </div>
         </div>
       </div>
